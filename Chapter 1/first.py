cdc45fd30f9070a357700a050cd32437afe3dbfe
sarahgonsalves223 <sarahgonsalves223@gmail.com>
2017-05-24 01:37:21 -0700

    Complete code for chp 1 exercises

--- Chapter 1/first.py
+++ Chapter 1/first.py
@@ -3,3 +3,20 @@ import survey
 table = survey.Pregnancies()
 table.ReadRecords()
 print('Number of pregnancies', len(table.records))
+
+liveBirthsFirst = 0
+liveBirthsOthers = 0
+pregWeeksFirst = 0
+pregWeeksOthers = 0
+for record in table.records:
+	if record.outcome == 1:
+		if record.birthord == 1:
+			liveBirthsFirst+=1
+			pregWeeksFirst+=record.prglength
+		else:
+			liveBirthsOthers+=1
+			pregWeeksOthers+=record.prglength
+print("Number of Live Births for first babies", liveBirthsFirst)
+print("Number of Live Births for other babies", liveBirthsOthers)
+print("Average Number of Pregnancy weeks for First babies", pregWeeksFirst/liveBirthsFirst)
+print("Average Number of Pregnancy weeks for Other babies", pregWeeksOthers/liveBirthsOthers)
